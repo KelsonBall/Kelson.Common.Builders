f3bbf93a7ad472db6011cf938461e79d676a74b3
Kelson Ball <kelson.j.ball@gmail.com>
2018-06-29 18:27:38 -0700

    Add base class

---
+++ Kelson.Common.Builders/Kelson.Common.Builders/ComposableBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Kelson.Common.Builders
+{
+    public abstract class ComposableBuilder<T> : IComposableBuilder<T>
+    {
+        public IComposableBuilder<T> Parent { get; }
+        public Func<T, T> Transform { get; }        
+
+        protected ComposableBuilder(Func<T> source = null)
+        {
+            Parent = null;
+            if (source != null)
+                Transform = (_ => source());
+            else
+                Transform = (_ => Activator.CreateInstance<T>());
+        }
+
+        protected ComposableBuilder(IComposableBuilder<T> parent, Func<T, T> transform)
+        {
+            Parent = parent;
+            Transform = transform;
+        }
+
+        public virtual T Build()
+        {            
+            if (Parent != null)
+                return Transform(Parent.Build());
+            else
+                return Transform(default);
+        }
+    }
+}
---
+++ Kelson.Common.Builders/Kelson.Common.Builders/IComposableBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kelson.Common.Builders
+{
+    public interface IComposableBuilder<T>
+    {
+        IComposableBuilder<T> Parent { get; }
+        Func<T, T> Transform { get; }
+        T Build();
+    }
+}
